7f3f5a97fd2f981790576c77d2ebbc1aea4e0a68
Thomas Toquothty <tltoquothty@gmail.com>
2024-12-29 19:15:29 -0500

    copilot files didn't work out of the box here. I thought of using lxml but the library is way more robust then what I needed. Also, being new with XML this would be good practice. The namespace piece was a major miss and my XML began working after that.

--- src/parsers/xml_parser.py
+++ src/parsers/xml_parser.py
@@ -1,22 +1,28 @@
 def parse_xml_data(xml_data):
     import xml.etree.ElementTree as ET
 
+    namespace = {"atom": "http://www.w3.org/2005/Atom"}
     root = ET.fromstring(xml_data)
+
+    entries = root.findall("atom:entry", namespaces=namespace)
     parsed_data = []
 
-    for item in root.findall('.//item'):
-        title = item.find('title').text
-        id = item.find('id').text
-        published_date = item.find('published_date').text
-        summary = item.find('summary').text
-        urls = [url.text for url in item.findall('url')]
+    for item in entries:
+        title = item.find("atom:title", namespaces=namespace).text
+        id = item.find("atom:id", namespaces=namespace).text
+        published_date = item.find("atom:published", namespaces=namespace).text
+        summary = item.find("atom:summary", namespaces=namespace).text
+        pdf_link = item.find("atom:link[@title='pdf']", namespaces=namespace)
+        url = pdf_link.attrib["href"]
 
-        parsed_data.append({
-            'title': title,
-            'id': id,
-            'published_date': published_date,
-            'summary': summary,
-            'urls': urls
-        })
+        parsed_data.append(
+            {
+                "title": title,
+                "id": id,
+                "published_date": published_date,
+                "summary": summary,
+                "url": url,
+            }
+        )
 
     return parsed_data
